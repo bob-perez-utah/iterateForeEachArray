1554d1b2c1ad34ae47d7a46e5297e28c40bec506
bob-perez-utah <bperez@gw.novell.com>
2018-08-29 20:06:54 -0600

    final commit

---
+++ iterateForEach.js
@@ -0,0 +1,12 @@
+    var numbers = [1,2,3,4,5,6,7,8,9,10];
+    var colors = ["red", "green", "blue", "orange"];
+    
+    numbers.forEach(function(color) {
+        if (color % 3 === 0) {
+            console.log("Number in array divisble by 3 = " + color);
+        }
+        
+    });
+
+
+
